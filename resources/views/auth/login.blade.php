4c3355d2705fe3e8f2def825777eb7badea6a394
Hansol <hansoltechnology@gmail.com>
2024-11-01 18:54:58 +0500

    new change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -98,24 +98,16 @@
                                 <h5 class="mt-3 text-muted">Sign in with</h5>
                                 <ul class="social-list list-inline mt-3 mb-0">
                                     <li class="list-inline-item">
-                                        <a href="javascript: void(0);"
-                                            class="social-list-item border-primary text-primary"><i
-                                                class="mdi mdi-facebook"></i></a>
+                                        <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
                                     </li>
                                     <li class="list-inline-item">
-                                        <a href="javascript: void(0);"
-                                            class="social-list-item border-danger text-danger"><i
-                                                class="mdi mdi-google"></i></a>
+                                        <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
                                     </li>
                                     <li class="list-inline-item">
-                                        <a href="javascript: void(0);"
-                                            class="social-list-item border-info text-info"><i
-                                                class="mdi mdi-twitter"></i></a>
+                                        <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
                                     </li>
                                     <li class="list-inline-item">
-                                        <a href="javascript: void(0);"
-                                            class="social-list-item border-secondary text-secondary"><i
-                                                class="mdi mdi-github"></i></a>
+                                        <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
                                     </li>
                                 </ul>
                             </div>
